dfd2122303b4a12c54180337981f685a78a7190c
mike <turnpikemike@gmail.com>
2013-01-29 14:38:22 -0500

    Added database connection.
    Now, it's just listing all transactions by date, in descending order.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <head>
 <?php include_once('../../inc/analyticstracking.php'); ?>
+<?php include_once('inc/connect.php'); ?>
 <?php 
 function initializeRoller(){
     $i = 0;
@@ -16,7 +17,26 @@ function initializeRoller(){
 
 <body>
     
+    <?php //Fetching from your database table.
+            $query = "SELECT * FROM $table ORDER BY `date` DESC";
+            $result = mysql_query($query);
+
+            if ($result) {
+                while($row = mysql_fetch_array($result)) {
+                    $name = $row["$field"];
+                    echo $row["date"] . " - $name Gallons<br>";
+                }
+            }
+    ?>
+    
     <form method="post" action="input.php">
+        <select name="year">
+        <?php initializeRoller(); ?>
+        </select>
+        
+        
+        
+        
         <select name="firstRoller">
         <?php initializeRoller(); ?>
         </select>
